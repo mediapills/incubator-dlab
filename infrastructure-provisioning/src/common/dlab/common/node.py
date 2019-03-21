f58766441b5175c46288bb1a1ab246f061f35eff
Andriy Yatskovets <andriy_yatskovets@epam.com>
2019-03-21 16:49:04 +0200

    Create common libs logger, env, config and fabric for python

---
+++ infrastructure-provisioning/src/common/dlab/common/node.py
@@ -0,0 +1,82 @@
+# *****************************************************************************
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# ******************************************************************************
+
+import abc
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseProcess:
+    # deploy + provision
+    def run(self):
+        pass
+
+    def terminate(self):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseService:
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseLibrariesManager:
+    def install_libraries(self):
+        pass
+
+    def show_libraries(self):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class SSNNode(BaseProcess):
+    pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class EDGENode(BaseProcess, BaseService):
+    pass
+    # get_status() -> do we need this ?
+    # recreate() -> ?upgrade? what cases
+    # reupload_key() -> need to be removed in SSHKeysManager
+
+
+@six.add_metaclass(abc.ABCMeta)
+class NotebookNode(BaseProcess, BaseService, BaseLibrariesManager):
+    pass
+    # Main function for configuring notebook server after deploying DataEngine service ?is it deploy?
+    # configure() -> ?what is inside?
+    # git_creds() -> needs to be removed in GITManaget
+
+
+@six.add_metaclass(abc.ABCMeta)
+class DataEngineNode(BaseProcess, BaseService, BaseLibrariesManager):
+    pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class DataEngineServerNode(BaseProcess, BaseLibrariesManager):
+    pass
---
+++ infrastructure-provisioning/src/common/dlab/common/shell.py
@@ -0,0 +1,45 @@
+# *****************************************************************************
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# ******************************************************************************
+
+from fabric import api as fab
+
+
+def sudo(command, shell=True, pty=True, combine_stderr=None, user=None):
+    """Run a shell command on a remote host, with superuser privileges."""
+    return fab.sudo(command, shell, pty, combine_stderr, user)
+
+
+def local(command, capture=False):
+    """Run a command on the local system."""
+    return fab.local(command, capture)
+
+
+def put(local_path=None, remote_path=None, use_sudo=False,
+        mirror_local_mode=False, mode=None):
+    """Upload one or more files to a remote host. As with the OpenSSH ``sftp``
+    program, `.put` will overwrite pre-existing remote files without requesting
+    confirmation."""
+    return fab.put(local_path, remote_path, use_sudo, mirror_local_mode, mode)
+
+
+def run(command, shell=True, pty=True, combine_stderr=None):
+    """Run a shell command on a remote host."""
+    fab.run(command, shell, pty, combine_stderr)
